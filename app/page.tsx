0ae95da30209206b01d90d73a82e32a36bf6dc90
michelzaum <michel.oliveira999@hotmail.com>
2024-12-27 21:55:26 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import { Header } from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 export default function Home() {
   return (
@@ -27,6 +30,27 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="border-solid- flex flex-col items-center justify-center border-l-2 px-5">
+              <p className="text-sm">Dezembro</p>
+              <p className="text-2xl">27</p>
+              <p className="text-sm">12:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
