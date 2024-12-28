c64f23e23f1c6f1ec617a8205837aaaedf411d53
michelzaum <michel.oliveira999@hotmail.com>
2024-12-28 00:13:34 -0300

    feat: add BarbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import { SearchIcon } from "lucide-react";
 import Image from "next/image";
+import { SearchIcon } from "lucide-react";
 import { Header } from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import { Card, CardContent } from "./_components/ui/card";
 import { Badge } from "./_components/ui/badge";
 import { Avatar, AvatarImage } from "./_components/ui/avatar";
+import { db } from "./_lib/prisma";
+import { BarbershopItem } from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  const barbsershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
@@ -31,7 +35,10 @@ export default function Home() {
           />
         </div>
 
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
@@ -51,6 +58,15 @@ export default function Home() {
             </div>
           </CardContent>
         </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbsershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
