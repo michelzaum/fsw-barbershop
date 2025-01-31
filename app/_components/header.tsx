954576b8591336b727d9847f0c31931db1ac90d6
michelzaum <michel.oliveira999@hotmail.com>
2025-01-30 22:19:00 -0300

    feat: creating a link in header to go to home page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,12 +4,15 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { Sheet, SheetTrigger } from "./ui/sheet";
 import { SidebarSheet } from "./sidebar-sheet";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        <Link href="/">
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button size="icon" variant="outline">
