bcadbce959c15e5f11b31e9d112d623ca3ec3260
michelzaum <michel.oliveira999@hotmail.com>
2025-02-13 23:21:54 -0300

    feat: ensure bookings are listed on bookings page

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -1,31 +1,56 @@
+import { Prisma } from "@prisma/client";
+import { isFuture, format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { Card, CardContent } from "./ui/card";
 
+interface BookingItemProps {
+  booking: Prisma.BookingGetPayload<{
+    include: {
+      service: {
+        include: {
+          barbershop: true;
+        };
+      };
+    };
+  }>;
+}
+
 // TODO: Receive booking information as props
-export const BookingItem = () => {
+export const BookingItem = ({ booking }: BookingItemProps) => {
+  const isConfirmed = isFuture(booking.date);
+
   return (
     <>
-      <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-        Agendamentos
-      </h2>
-      <Card>
+      <Card className="min-w-[90%]">
         <CardContent className="flex justify-between p-0">
           <div className="flex flex-col gap-2 py-5 pl-5">
-            <Badge className="w-fit">Confirmado</Badge>
-            <h3 className="font-semibold">Corte de cabelo</h3>
+            <Badge
+              className="w-fit"
+              variant={isConfirmed ? "default" : "secondary"}
+            >
+              {isConfirmed ? "Confirmado" : "Finalizado"}
+            </Badge>
+            <h3 className="font-semibold">{booking.service.name}</h3>
             <div className="flex items-center gap-2">
               <Avatar className="h-6 w-6">
-                <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+                <AvatarImage src={booking.service.barbershop.imageUrl} />
               </Avatar>
-              <p className="text-sm">Barbearia FSW</p>
+              <p className="text-sm">{booking.service.barbershop.name}</p>
             </div>
           </div>
 
           <div className="border-solid- flex flex-col items-center justify-center border-l-2 px-5">
-            <p className="text-sm">Dezembro</p>
-            <p className="text-2xl">27</p>
-            <p className="text-sm">12:00</p>
+            <p className="text-sm capitalize">
+              {format(booking.date, "MMM", { locale: ptBR })}
+            </p>
+            <p className="text-2xl">
+              {format(booking.date, "dd", { locale: ptBR })}
+            </p>
+            <p className="text-sm">
+              {format(booking.date, "HH:mm", { locale: ptBR })}
+            </p>
           </div>
         </CardContent>
       </Card>
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,44 @@
 import Image from "next/image";
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { db } from "./_lib/prisma";
 import { Header } from "./_components/header";
 import { Button } from "./_components/ui/button";
-import { db } from "./_lib/prisma";
 import { BarbershopItem } from "./_components/barbershop-item";
 import { quickSearchOptions } from "./_constants/quickSearch";
 import { BookingItem } from "./_components/booking-item";
 import Search from "./_components/search";
 import { Sheet, SheetClose } from "./_components/ui/sheet";
+import { authOptions } from "./_lib/auth";
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
   const barbsershops = await db.barbershop.findMany({});
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   });
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : [];
 
   return (
     <div>
@@ -57,7 +80,14 @@ export default async function Home() {
           />
         </div>
 
-        <BookingItem />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
