a3da9409b1fd8277632071ea41b38ac77105fd67
michelzaum <michel.oliveira999@hotmail.com>
2025-02-15 23:20:29 -0300

    refactor: ensure decimal warning is not shown on console

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -61,7 +61,10 @@ const Bookings = async () => {
               Confirmados
             </h2>
             {confirmedBookings.map((booking) => (
-              <BookingItem key={booking.id} booking={booking} />
+              <BookingItem
+                key={booking.id}
+                booking={JSON.parse(JSON.stringify(booking))}
+              />
             ))}
           </>
         )}
