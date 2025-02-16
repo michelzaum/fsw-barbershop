16ce7aeb4afa023103580380eb190f733635747d
michelzaum <michel.oliveira999@hotmail.com>
2025-02-16 01:40:20 -0300

    feat: ensure a feeback message is displayed if the user has no bookings

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,9 +1,11 @@
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 import { db } from "../_lib/prisma";
 import { Header } from "../_components/header";
 import { authOptions } from "../_lib/auth";
 import { BookingItem } from "../_components/booking-item";
+import { Button } from "../_components/ui/button";
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions);
@@ -55,6 +57,18 @@ const Bookings = async () => {
       <Header />
       <div className="space-y-3 p-5">
         <h1 className="text-xl font-bold">Agendamentos</h1>
+        {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
+          <div className="flex w-full flex-col gap-6">
+            <p className="text-gray-400">
+              Você não tem agendamentos no momento.
+            </p>
+            <Button variant="secondary">
+              <Link href="/" className="w-full">
+                Voltar para o início
+              </Link>
+            </Button>
+          </div>
+        )}
         {confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
