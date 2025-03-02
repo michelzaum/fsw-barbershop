80bcdde3642e3e1b6185cc685e15a7f20edac5d9
michelzaum <michel.oliveira999@hotmail.com>
2025-03-02 12:13:56 -0300

    refactor: add data function to get a barbershop by id

---
+++ app/_data/get-barbershop-by-id.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { db } from "../_lib/prisma";
+
+export const getBarbershopById = async (id: string) => {
+  return db.barbershop.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      services: true,
+    },
+  });
+};
